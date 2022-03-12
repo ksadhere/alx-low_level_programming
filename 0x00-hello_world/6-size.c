09e34ef1529823536800f3c8f80a1955a9a3819f
Kevin Sadhere <kevinsadhere@gmail.com>
2022-03-12 00:33:52 +0000

    Added solution to task 6~

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	return (0);
+}
