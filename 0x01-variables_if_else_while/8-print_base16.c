33f8e8860182b16469e1264621a92dc17ee1f499
Kevin Sadhere <kevinsadhere@gmail.com>
2022-03-13 22:43:59 +0000

    Added solution to task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	char i;
+	char j;
+
+	i = 48;
+	j = 'a';
+
+	while (i <= 58)
+	{
+	putchar(i);
+	i++;
+	}
+	while (i <= 'f')
+	{
+	putchar(i);
+	i++;
+	}
+	putchar('\n');
+	return (0);
+}
