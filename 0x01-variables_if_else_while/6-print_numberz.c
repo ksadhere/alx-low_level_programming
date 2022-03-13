5f7ae46c3fc2c4f264fb73f317d592206d634fdb
Kevin Sadhere <kevinsadhere@gmail.com>
2022-03-13 22:32:02 +0000

    Added solutuion to task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+	putchar(i);
+	i++;
+	}
+	putchar('\n');
+	return (0);
+}
