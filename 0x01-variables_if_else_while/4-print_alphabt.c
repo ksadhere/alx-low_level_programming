bd4334610bacc38c4eec86014f644e0ccecec0e2
Kevin Sadhere <kevinsadhere@gmail.com>
2022-03-13 22:08:59 +0000

    Added solution to task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	i = 'a';
+
+	while (i <= 'z')
+	{
+	if (i != 'q' && i != 'e')
+	{
+		putchar(i);
+		i++
+	}
+	}
+	putchar('\n');
+	return (0);
+}
